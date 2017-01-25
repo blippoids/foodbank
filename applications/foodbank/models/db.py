65b081360287481a06f7a29df26243f87067a535
ljyanesm <yanes.luis@gmail.com>
2017-01-25 23:37:38 +0000

    Changing db to correct sqlite

--- applications/foodbank/models/db.py
+++ applications/foodbank/models/db.py
@@ -24,30 +24,6 @@ from gluon.contrib.appconfig import AppConfig
 # -------------------------------------------------------------------------
 myconf = AppConfig(reload=True)
 
-if not request.env.web2py_runtime_gae:
-    # ---------------------------------------------------------------------
-    # if NOT running on Google App Engine use SQLite or other DB
-    # ---------------------------------------------------------------------
-    db = DAL(myconf.get('db.uri'),
-             pool_size=myconf.get('db.pool_size'),
-             migrate_enabled=myconf.get('db.migrate'),
-             check_reserved=['all'])
-else:
-    # ---------------------------------------------------------------------
-    # connect to Google BigTable (optional 'google:datastore://namespace')
-    # ---------------------------------------------------------------------
-    db = DAL('google:datastore+ndb')
-    # ---------------------------------------------------------------------
-    # store sessions and tickets there
-    # ---------------------------------------------------------------------
-    session.connect(request, response, db=db)
-    # ---------------------------------------------------------------------
-    # or store session in Memcache, Redis, etc.
-    # from gluon.contrib.memdb import MEMDB
-    # from google.appengine.api.memcache import Client
-    # session.connect(request, response, db = MEMDB(Client()))
-    # ---------------------------------------------------------------------
-
 # -------------------------------------------------------------------------
 # by default give a view/generic.extension to all actions from localhost
 # none otherwise. a pattern can be 'controller/function.extension'
@@ -130,12 +106,6 @@ auth.settings.reset_password_requires_verification = True
 # -------------------------------------------------------------------------
 # after defining tables, uncomment below to enable auditing
 # -------------------------------------------------------------------------
-# auth.enable_record_versioning(db)
-
-
-from gluon.tools import Auth
-
-db = DAL("sqlite://storage.sqlite")
 
 db.define_table("category",
                 Field("name", "string"),
