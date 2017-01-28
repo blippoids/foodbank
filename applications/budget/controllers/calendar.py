753f7ef07c033f922f66b284a83a1596bd83f9cf
ljyanesm <yanes.luis@gmail.com>
2017-01-28 02:01:25 +0000

    Delelting apps

--- applications/budget/controllers/calendar.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# try something like
-def list_transactions():
-    ab = db(db.tt.id==db.td.tt_id).select(db.td.bleh, db.tt.name)
-    return dict(data=ab)
--- applications/budget/controllers/transaction.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# try something like
-def transaction_list():
-    data = SQLFORM(db.trns)
-
-    if data.process().accepted:
-        response.flash = "inserted"
-    return dict(data=data)
